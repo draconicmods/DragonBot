52564f7c4acc2d5f6d3e722da6e81249be8cadc4
adran <30971811+Adran-Marit@users.noreply.github.com>
2020-08-25 22:41:22 +1000

    Update projects.js

--- Modules/projects.js
+++ Modules/projects.js
@@ -17,7 +17,6 @@ const embed = new Discord.RichEmbed()
   .addField('?dmmc','**- This command links dmmc project**')
   .addField('?ddsi','**- This command links to the ddsi project**') 
   //.addBlankField()
-  //.setImage('https://media.discordapp.net/attachments/516367045189959681/543270649901088778/DI_BANNER2.png')
   .setTimestamp()
   .setFooter('https://draconicmods.com/', 'https://media.discordapp.net/attachments/488214232203264001/651205690773340199/image0.png');
 module.exports = (embed);
