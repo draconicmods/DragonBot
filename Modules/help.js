c7d07407e86d0a2eaa918cb0b137e7cce5fb4d0a
adran <30971811+Adran-Marit@users.noreply.github.com>
2020-08-25 22:41:41 +1000

    Update help.js

--- Modules/help.js
+++ Modules/help.js
@@ -20,7 +20,6 @@ const embed = new Discord.RichEmbed()
   .addField('?projects', '**- This commands links to the Projects embed**')
   .addField('?rave', '**- It\'s rave time!!!**')
   //.addBlankField()
-  //.setImage('https://media.discordapp.net/attachments/516367045189959681/543270649901088778/DI_BANNER2.png')
   .setTimestamp()
   .setFooter('https://draconicmods.com/', 'https://media.discordapp.net/attachments/488214232203264001/651205690773340199/image0.png');
 module.exports = (embed);
