8eff685f5996fe64035f49e80bd9d299287b5c48
adran <30971811+Adran-Marit@users.noreply.github.com>
2021-04-05 20:06:17 +1000

    Update app.js
    removed unneeded sections

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ Version 3.0.0
 */
 
 /*
-Slowly implementing decomminsing of dragonbot 5-April-2021
+Slowly implementing decommissioning of dragonbot 5-April-2021
 */
 
 /* Requirements */
@@ -15,11 +15,8 @@ const token = require('./settings.json').token;
 /* Modules Section */ 
 const ap = require('./Modules/anti-piracy.js')
 const credits = require('./Modules/credits.js')
-//const dmmc = require('./Modules/dmmc.js')
-//const ddsi = require('./Modules/ddsi.js')
 const faq = require('./Modules/faq.js')
 const help = require('./Modules/help.js')
-//const projects = require('./Modules/projects.js')
 
 /* Loggin the bot in and setting its activity */
 client.on('ready',() => {
@@ -37,20 +34,11 @@ client.on('message', message => {
     if(message.content == ("?credits")){
         message.reply(credits);
 
- //   } else if(message.content == ("?dmmc")){
- //       message.reply(dmmc);
-
- //  } else if(message.content == ("?ddsi")){
- //       message.reply(ddsi);
-
     } else if (message.content == ("?help")){
         message.reply(help);
     
     } else if(message.content == ("?faq")){
         message.reply(faq);
-    
- //   } else if(message.content == ("?projects")){
- //       message.reply(projects);  
         
     } else if 
     (message.reply("Please enter `?help` for a list of commands."));
@@ -65,11 +53,9 @@ client.on('message', message => {
     }
 
 //Moved AP and assosiated regex here in 3.0.1 so mods can trigger AP section
+//Sets regex constant for phrase(s) and replies.    
 const piracy = message.content.match(new RegExp (/^(?=.*\b(sx|pirate|nsp|rcm|atmo|sxos|yuzu|xci|modchip|atmosphere|reinx|cfw|nro|hekate|emummc|nsz|tinfoil|goldleaf|jits|jack ?in ?the ?shop|tx|team ?xecuter|hbg|hbg ?shop|freeshop|free ?shop)\b).*$/i));
-//Sets regex constant for phrase(s) and replies.
-//const preorder = message.content.match(new RegExp (/^(?=.*\b(dragon ?mmc|dmmc|dsi| dragon ?dsi)\b)(?=.*\b(preorder|pre-order)\b).*$/i));
-//const cost = message.content.match(new RegExp(/^(?=.*\b(dragon ?mmc|dmmc|dsi| dragon ?dsi)\b)(?=.*\b(cost|price)\b).*$/i));
-//const commands = message.content.match(new RegExp (/^(?=.*\b(dragon ?bot|db|bot|dragonbot)\b)(?=.*\b(help|halp)\b).*$/i));
+
     
 //this is here so anyone can trigger the commands
  if(piracy) {
@@ -81,10 +67,6 @@ const piracy = message.content.match(new RegExp (/^(?=.*\b(sx|pirate|nsp|rcm|atm
     message.react('651624619920588820');
     message.author.send(credit);
 
-/* } else if(message.content == ("?dmmc")){
-    message.react('651624619920588820');
-    message.author.send(dmmc);
-*/
 } else if(message.content == ("?faq")){
     message.react('651624619920588820');
     message.author.send(faq);
@@ -93,35 +75,8 @@ const piracy = message.content.match(new RegExp (/^(?=.*\b(sx|pirate|nsp|rcm|atm
     message.react('651624619920588820');
     message.author.send(help);
 
-} else if(message.content == ("?projects")){
-    message.react('651624619920588820');
-    message.author.send(projects);
-
 } else if (message.content === '?rave') {
         message.author.send(`<@${message.author.id}>, IT'S RAVE TIME!`, {files: ['https://media1.tenor.com/images/e19a05faf32c511572acd08a38bebdd6/tenor.gif']});
     }
-
-//THIS SECTION IS CURRENTLY BROKEN
-
-   /* 
-    
-//Ignores users if they have one of the following roles for the regex
-if (! message.member || message.member.roles.find('name', 'Creator') || message.member.roles.find('name', 'The Fuzz') || message.member.roles.find('name', 'Certified Madlad')) {
-        return;
-}
-      
-    if(preorder) {  
-    message.react('651624619920588820')
-    message.channel.send(`<@${message.author.id}>, Sorry MatinatorX doesn't believe in preorders`);
-}   
-    else if(cost) {  
-    message.react('651624619920588820')
-    message.channel.send(`<@${message.author.id}>, Cost is yet to be announced. Keep an eye on <#489199851171610649>`);
-}
-    else if(commands) {
-    message.react('651624619920588820')  
-    message.author.send(help);
-}
-*/
 });
 client.login(token);
